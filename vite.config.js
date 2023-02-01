d4e695ccaab477ce4c5262cc8a08aadfcaa18f36
reginx <reginx@qq.com>
2023-02-01 20:09:48 +0800

    es5

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,25 @@
+import legacy from '@vitejs/plugin-legacy'
+
 export default {
-    build: {
-        rollupOptions: {
-          output: {
-            entryFileNames: `[name].js`,
-            assetFileNames: `[name].[ext]`
-          }
-        }
-      }
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        //生产环境时移除console
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`,
+      },
+    },
+  },
+  plugins: [
+    legacy({
+      targets: ['chrome 52'],
+    }),
+  ],
 }
